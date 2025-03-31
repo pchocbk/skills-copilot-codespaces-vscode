b727145989a20b090bc149c567bb75b0276d9072
PhucDut <101220291@sv1.dut.udn.vn>
2025-03-31 17:08:40 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,7 @@
+// Create web server
+
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;          
